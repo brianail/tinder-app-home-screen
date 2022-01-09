6c493587cfd493b3d7e95edd8258c138fe2d7292
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-09 17:12:08 -0300

    creating an extension of UIView to fill the super view

---
+++ TinderApp/Extensions/Constraints.swift
@@ -0,0 +1,21 @@
+//
+//  Constraints.swift
+//  TinderApp
+//
+//  Created by Giovanna Moeller on 09/01/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+  func fillSuperView() {
+    self.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      self.topAnchor.constraint(equalTo: self.superview!.topAnchor),
+      self.leadingAnchor.constraint(equalTo: self.superview!.leadingAnchor),
+      self.bottomAnchor.constraint(equalTo: self.superview!.bottomAnchor),
+      self.trailingAnchor.constraint(equalTo: self.superview!.trailingAnchor)
+    ])
+  }
+}
