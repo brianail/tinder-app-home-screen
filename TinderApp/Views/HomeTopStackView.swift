f4c1c96c87cbdb9b2812665291050c6c7c4225e5
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-09 15:28:42 -0300

    adding new icons to assets catalog

--- TinderApp/Views/HomeTopStackView.swift
+++ TinderApp/Views/HomeTopStackView.swift
@@ -10,13 +10,17 @@ import UIKit
 class HomeTopStackView: UIStackView {
 
   override init(frame: CGRect) {
+    
     super.init(frame: frame)
+    
     let topStackViewSubviews: [UIView] = [UIColor.black, .systemPink, .magenta].map { color -> UIView in
       let view = UIView()
       view.backgroundColor = color
       return view
     }
+    
     topStackViewSubviews.forEach(addArrangedSubview(_:))
+    
     self.distribution = .fillEqually
     self.heightAnchor.constraint(equalToConstant: 120).isActive = true
   }
